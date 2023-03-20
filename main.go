367d6b45a52a8e04043f4a7fd12834c185cf466c
Rodrigo Noblega <rodrigonoblega@Rodrigos-MacBook-Pro.local>
2023-03-20 11:03:03 -0300

    Solution using channels

--- main.go
+++ main.go
@@ -9,69 +9,80 @@ import (
 )
 
 type StatusInfo struct {
-	executingTimePtr    *time.Time
-	isExecutingATaskPtr *bool
+	executingTime    time.Time
+	isExecutingATask bool
 }
 
 func main() {
-	//I have to use Mutex because I'll be reading/updating StatusInfo struct from different routines
-	var mu sync.Mutex
-	statusInfo := StatusInfo{}
+	statusInfo := &StatusInfo{}
+	statusInfoChannel := make(chan StatusInfo, 1)
 
 	//Subgoroutine - loop to get task and process it
-	go func() {
-		executingTime := time.Now()
-		isExecutingATask := false
-
-		mu.Lock()
-		statusInfo.executingTimePtr = &executingTime
-		statusInfo.isExecutingATaskPtr = &isExecutingATask
-		mu.Unlock()
-
-		for {
-			executingTime = time.Now()
-			isExecutingATask = true
-
-			fmt.Println("Changing status info to notify that I'm executing a task")
-			mu.Lock()
-			statusInfo.executingTimePtr = &executingTime
-			statusInfo.isExecutingATaskPtr = &isExecutingATask
-			mu.Unlock()
-
-			//This represents the execution of a test
-			fmt.Println("I'm executing a task...")
-			time.Sleep(10 * time.Second)
-
-			isExecutingATask = false
-			executingTime = time.Now()
-
-			fmt.Println("Changing status info to notify that I'm not executing any task")
-			mu.Lock()
-			statusInfo.executingTimePtr = &executingTime
-			statusInfo.isExecutingATaskPtr = &isExecutingATask
-			mu.Unlock()
-
-			time.Sleep(2 * time.Second)
-		}
-	}()
+	go loopToProcessTask(statusInfoChannel)
+	go loopToUpdateStatusInfo(statusInfo, statusInfoChannel)
 
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
-		mu.Lock()
-		executingTime := *statusInfo.executingTimePtr
-		isExecutingATask := *statusInfo.isExecutingATaskPtr
-		mu.Unlock()
 		var message string
-		if !isExecutingATask {
+		var mu sync.Mutex
+		mu.Lock()
+		currentStatusInfo := statusInfo
+		mu.Unlock()
+
+		if !currentStatusInfo.isExecutingATask {
 			message = fmt.Sprintf("There aren't tasks running")
 		} else {
-			message = fmt.Sprintf("Executing time is taking: %s", time.Since(executingTime))
+			message = fmt.Sprintf("Executing time is taking: %s", time.Since(currentStatusInfo.executingTime))
 		}
 
 		c.JSON(http.StatusOK, gin.H{
-			"isExecuting": isExecutingATask,
+			"isExecuting": statusInfo.isExecutingATask,
 			"message":     message,
 		})
 	})
 	r.Run()
+}
+
+func loopToUpdateStatusInfo(statusInfo *StatusInfo, statusInfoChannel chan StatusInfo) {
+	var mu sync.Mutex
+	for {
+		select {
+		case statusOfChannel := <-statusInfoChannel:
+			if !statusOfChannel.isExecutingATask {
+				fmt.Println("Updating the channel notifying that there's no task being executing")
+			} else {
+
+				fmt.Println("Updating the channel notifying that there's a task being executing")
+			}
+			mu.Lock()
+			statusInfo.executingTime = statusOfChannel.executingTime
+			statusInfo.isExecutingATask = statusOfChannel.isExecutingATask
+			mu.Unlock()
+		}
+	}
+}
+
+func loopToProcessTask(statusInfoChannel chan StatusInfo) {
+
+	statusInfo := StatusInfo{executingTime: time.Now(), isExecutingATask: false}
+	statusInfoChannel <- statusInfo
+
+	for {
+
+		fmt.Println("Changing status info to notify that I'm executing a task")
+		statusInfo.executingTime = time.Now()
+		statusInfo.isExecutingATask = true
+		statusInfoChannel <- statusInfo
+
+		//This represents the execution of a test
+		fmt.Println("I'm executing a task...")
+		time.Sleep(10 * time.Second)
+
+		fmt.Println("Changing status info to notify that I'm not executing any task")
+		statusInfo.executingTime = time.Now()
+		statusInfo.isExecutingATask = false
+		statusInfoChannel <- statusInfo
+
+		time.Sleep(2 * time.Second)
+	}
 }
